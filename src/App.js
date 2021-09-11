d4b4b06c940827ad756e4f0cbffc53a467fec671
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-09-11 22:11:42 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Compressor from './pages/Compressor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>웹피피 - 이미지 압축기</h1>
+      <Compressor/>
     </div>
   );
 }
+
+
 
 export default App;
