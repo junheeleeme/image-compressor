b1c9d1d94393287bc95660be86d5cb8489bae03a
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-09-12 02:16:00 +0900

    업로드 리스트 표시

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import styled from 'styled-components';
+import Header from './common/Header';
+import Footer from './common/Footer';
+import Main from './Layout/Main';
 import Compressor from './pages/Compressor';
+
+const AppStyled = styled.div`
+  position: relative;
+  max-width: 1080px;
+  min-height: 100vh;
+`
 
 function App() {
   return (
-    <div className="App">
-      <h1>웹피피 - 이미지 압축기</h1>
-      <Compressor/>
-    </div>
+    <AppStyled className="App">
+      <Header/>
+        <Main>
+          <Compressor/>
+        </Main>
+      <Footer/>
+    </AppStyled>
   );
 }
 
