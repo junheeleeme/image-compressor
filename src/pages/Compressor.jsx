6e948b07c451406f9bf84bcf216bb6e7cb2224f7
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2021-09-13 18:39:26 +0900

    압축 후 파일 변경 및 여러 이미지일 경우 압축 후 전체 다운로드 기능 추가

--- src/pages/Compressor.jsx
+++ src/pages/Compressor.jsx
@@ -7,14 +7,14 @@ import FileSaver from "file-saver"
 
 export default function Compressor(){
 
-    const [imageLi, setImageLi] = useState([
+    const [imageLi, setImageLi] = useState([ //렌더링을 위한 state
         // {
         //     title : '',
         //     type : '',
         //     size : 0,
         // },
     ]);
-    const [doneLi, setDoneLi] = useState([
+    const [doneLi, setDoneLi] = useState([ //렌더링을 위한 state
                 // {
         //     title : '',
         //     type : '',
@@ -22,15 +22,17 @@ export default function Compressor(){
         //     link : '',
         // },
     ]);
-
-    useEffect(()=> {
-        console.log(imageLi);
-    }, [imageLi])
-    useEffect(()=> {
-        console.log('완료' +doneLi);
-    }, [doneLi])
-
+    const [isAllDown, setIsAllDown] = useState(false);
     const dragArea = useRef(null);
+    const Alldown = useRef(null);
+
+    useEffect(()=> { 
+        console.log(imageLi); 
+        setIsAllDown(false);
+        setDoneLi([]);
+    }, [imageLi]);
+
+    useEffect(()=> { console.log('완료' +doneLi); }, [doneLi])
 
     const preventDefaults = (e) => { //이벤트 방지
         e.preventDefault();
@@ -70,6 +72,7 @@ export default function Compressor(){
                 })
             });
             setImageLi(fileList); //전체 파일 리스트에 렌더링
+            
             InputHandler(files); //핸들러에 파일 리스트 전달
         }
         
@@ -79,11 +82,11 @@ export default function Compressor(){
 
         if(!file[1]){ //파일이 하나일 경우
             const res = await compressImage(file);
-            
+            const type = res.type.substr(6, 5); //이미지 확장자
             setDoneLi([
                 {
-                    title : res.name,
-                    type : res.type.substr(6, 5),
+                    title : res.name.slice(0, -type.length) + '_webpp.' + type, //압축 후 이미지 파일명 변경
+                    type : type,
                     size : getfileSize(res.size),
                     link : URL.createObjectURL(res),
                 }
@@ -94,19 +97,20 @@ export default function Compressor(){
             const zipList = [];
             for(let i=0 ; i < file.length ; i++){  //비동기로 복수의 이미지 압축 후 배열에 추가
                 const res = await compressImage(file[i]);
-                zipList.push(res);
-                compressedItems.push(
+                const type = res.type.substr(6, 5); //이미지 확장자
+                
+                zipList.push(res); //압축을 위해 다른 배열에 저장
+                compressedItems.push( 
                     {
-                        title : res.name,
-                        type : res.type.substr(6, 5),
+                        title : res.name.slice(0, -type.length) + '_webpp.' + type, //압축 후 이미지 파일명 변경
+                        type : type,
                         size : getfileSize(res.size),
                         link : URL.createObjectURL(res),
                     }
                 );
             }
-
-            setDoneLi(compressedItems);   
             CreateZip(zipList);
+            setDoneLi(compressedItems);  
         }   
     }
 
@@ -120,84 +124,86 @@ export default function Compressor(){
         } catch(e){ console.log(e); }
     }
 
-    const getfileSize = (x) => {
+    const getfileSize = (x) => { //파일 사이즈 표현
         var s = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
         var e = Math.floor(Math.log(x) / Math.log(1024));
         return (x / Math.pow(1024, e)).toFixed(2) + " " + s[e];
     };
 
-    const CreateZip = (imgs) => {
-
+    const CreateZip = (imgs) => { //압축 만들기
         const zip = new JSZip();
-
         imgs.forEach(img => {
             zip.folder("webpp-images").file(img.name, img);
         });
-        
-
         zip.generateAsync({type:"blob"})
-        .then((content) => {
-            FileSaver(content, "webpp.zip");
+        .then((imgs) => {
+            Alldown.current.download = 'webpp.zip';  // 다운로드 zip파일 이름
+            Alldown.current.href = URL.createObjectURL(imgs);
+            setIsAllDown(true);
         });
-
     }
 
     return(
         <>
-        <button onClick={CreateZip}>다운로드</button>
-            <CompressorDrag>
-                <div className="drag-area" ref={dragArea} onDragEnter={DragHighlight} onDragOver={DragHighlight} onDragLeave={unDragHighlight} onDrop={DropFile}/>
-                <div className="dragIcon" onDragEnter={DragHighlight} onDragOver={DragHighlight} onDragLeave={unDragHighlight} onDrop={DropFile}>
-                        <box-icon name='image' type='solid' color='#ffffff' />
-                        <p className="ImgEx1">Select Image File</p>
-                        <p className="ImgEx2">&#38;</p>
-                        <p className="ImgEx3">Drag &#38; Drop Your Images</p>
+        <CompressorDrag>
+            <div className="drag-area" ref={dragArea} onDragEnter={DragHighlight} onDragOver={DragHighlight} onDragLeave={unDragHighlight} onDrop={DropFile}/>
+            <div className="dragIcon" onDragEnter={DragHighlight} onDragOver={DragHighlight} onDragLeave={unDragHighlight} onDrop={DropFile}>
+                    <box-icon name='image' type='solid' color='#ffffff' />
+                    <p className="ImgEx1">Select Image File</p>
+                    <p className="ImgEx2">&#38;</p>
+                    <p className="ImgEx3">Drag &#38; Drop Your Images</p>
+            </div>
+        </CompressorDrag>
+        <CompressorList>
+            <div className="list-area">
+                <div className="left">
+                    <ul>
+                    <li style={{textAlign: 'center', fontSize: '15px'}}>
+                        <span style={{fontSize : '15px'}} className="img-no">No</span>
+                        <span style={{fontSize : '15px'}} className="img-title">Uploaded File Name</span>
+                        <span style={{fontSize : '15px'}} className="img-size">Size</span>
+                    </li>
+                    {   
+                        imageLi.map((li, idx)=> 
+                            <li key={li.title + idx}>
+                                <span className="img-no">{idx+1}</span>
+                                <span className="img-title" title={li.title}>{li.title}</span>
+                                <span className="img-size">{li.size}</span>
+                            </li>
+                        )
+                    }
+                    </ul>
                 </div>
-            </CompressorDrag>
-            <CompressorList>
-                <div className="list-area">
-                    <div className="left">
-                        <ul>
-                        <li style={{textAlign: 'center', fontSize: '15px' , fontWeight : 'bold'}}>
-                            <span style={{fontSize : '16px'}} className="img-no">No</span>
-                            <span style={{fontSize : '16px'}} className="img-title">Uploaded File Name</span>
-                            <span style={{fontSize : '16px'}} className="img-size">Size</span>
+                <div className="right">
+                    <ul>
+                        <li style={{textAlign: 'center'}}>
+                            <span style={{fontSize : '15px'}} className="img-no-done">No</span>
+                            <span style={{fontSize : '15px'}} className="img-title-done">Compressed File Name</span>
+                            <span style={{fontSize : '15px'}} className="img-size-done">Size</span>
+                            <span style={{fontSize : '15px', padding : '7px'}} title="Download" className="img-down-done">D</span>
                         </li>
-                        {   
-                            imageLi.map((li, idx)=> 
+                        {  
+                            doneLi.map((li, idx)=> 
                                 <li key={li.title + idx}>
-                                    <span className="img-no">{idx+1}</span>
-                                    <span className="img-title" title={li.title}>{li.title}</span>
-                                    <span className="img-size">{li.size}</span>
+                                    <span className="img-no-done">{idx+1}</span>
+                                    <span className="img-title-done" title={li.title}>{li.title}</span>
+                                    <span className="img-size-done">{li.size}</span>   
+                                    <span className="img-down-done"><a href={li.link} download={li.title}><box-icon type='solid' name='cloud-download'/></a></span>
                                 </li>
                             )
                         }
-                        </ul>
-                    </div>
-                    <div className="right">
-                        <ul>
-                            <ul>
-                                <li style={{textAlign: 'center', fontWeight : 'bold'}}>
-                                    <span style={{fontSize : '16px'}} className="img-no-done">No</span>
-                                    <span style={{fontSize : '16px'}} className="img-title-done">Compressed File Name</span>
-                                    <span style={{fontSize : '16px'}} className="img-size-done">Size</span>
-                                    <span style={{fontSize : '16px', padding : '7px'}} className="img-down-done">D</span>
-                                </li>
-                                {  
-                                    doneLi.map((li, idx)=> 
-                                        <li key={li.title + idx}>
-                                            <span className="img-no-done">{idx+1}</span>
-                                            <span className="img-title-done" title={li.title}>{li.title}</span>
-                                            <span className="img-size-done">{li.size}</span>   
-                                            <span className="img-down-done"><a href={li.link} download={li.title}><box-icon type='solid' name='cloud-download'/></a></span>
-                                        </li>
-                                    )
-                                }
-                            </ul>
-                        </ul>
-                    </div>                    
-                </div>
-            </CompressorList>
+                    </ul>
+                    {
+                        !isAllDown
+                            ?
+                        <a ref={Alldown} href='/' className="AllDown" style={{display: 'none'}}>전체 다운로드</a>
+                            : 
+                        <a ref={Alldown} href='/' className="AllDown">전체 다운로드</a>
+                    }
+                    
+                </div>                    
+            </div>
+        </CompressorList>
         </>
     )
 }
@@ -224,17 +230,24 @@ const CompressorDrag = styled.section`
 
 const CompressorList = styled.section`
     position: relative; height: 300px;
+    margin: auto;
     overflow: hidden; border: 1px solid #c7c7c7;
     border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; 
+    @media screen and (max-width: 600px){
+        & { height: auto; }
+    }
     & .list-area{
         height: 100%;
-        & div{
+        & .left, .right{
             position: relative; display: inline-block; 
             width: 50%; height: 100%; vertical-align: top;
         }
         & .left{ border-right: 1px solid #c7c7c7; }
+        @media screen and (max-width: 600px){
+            .left, .right{ width: 100%; height:150px; }
+        }
         & ul{
-            width: 100%; height: 100%; background-color: #fff;
+            width: 100%; height: 100%; background-color: #fff; overflow: auto;
             & li{ 
                 position: relative; display: block;  border-bottom: 1px solid #c7c7c7;
                 line-height: 1;
@@ -258,6 +271,18 @@ const CompressorList = styled.section`
             }
         }
         & .right ul>li:first-child{ animation: none; }
+        & .right ul{
+
+            @media screen and (max-width: 600px){
+                border-top: 1px solid #9f9f9f;
+            }
+        }
         & .right li{ animation: done 1s; }
+        & .right .AllDown{ 
+            position: absolute; bottom: -100px; left: 50%; transform: translate(-50%, 0);
+            padding: 5px 20px; background-color: rgba(0,0,0, 0.4); border-radius: 7px; color: #fff;
+            font-size: 14px;
+            animation: slideUp 1s forwards;
+        }
     }
 `
